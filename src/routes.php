07a1cea40435e65d38e50ddb443e9f8753e4087c
Yaro <12fcv4@gmail.com>
2015-01-20 18:02:49 +0200

    add routes for file/dir creation

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,8 @@ Route::post('/moe/file/save', 'Yaro\Mecha\MoeController@doSaveFileContents');
 Route::post('/moe/file/move', 'Yaro\Mecha\MoeController@doMoveFiles');
 Route::post('/moe/file/copy', 'Yaro\Mecha\MoeController@doCopyFiles');
 Route::post('/moe/file/remove', 'Yaro\Mecha\MoeController@doRemoveFile');
+Route::post('/moe/file/create', 'Yaro\Mecha\MoeController@doCreateFile');
+Route::post('/moe/dir/create', 'Yaro\Mecha\MoeController@doCreateDir');
 Route::post('/moe/tree/connector', 'Yaro\Mecha\MoeController@getTreeContents');
 Route::post('/moe/auth/check', 'Yaro\Mecha\MoeController@doAuthenticate');
 
